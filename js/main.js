104a70bf2fa86cb6a01328332f2bbfcbc1f1feb0
davidearbola <davidearbola@gmail.com>
2024-05-07 17:16:20 +0200

    M2 M3 - gestione al click del cambio delle slides

--- js/main.js
+++ js/main.js
@@ -29,14 +29,12 @@ let slides = [
 
 let slide = document.getElementById(`slide`);
 
-slides.forEach((elemento) => {
-	slide.innerHTML = `
-    <div class="row">
+slide.innerHTML = `<div class="row">
     <div class="col-img">
-        <img src="./img/${elemento.percorso}" alt="">
+        <img src="./img/${slides[0].percorso}" alt="">
         <div class="absolute">
-            <h4>${elemento.titolo}</h4>
-            <p>${elemento.descrizione}</p>
+            <h4>${slides[0].titolo}</h4>
+            <p>${slides[0].descrizione}</p>
         </div>
     </div>
     <div class="col-thumb">
@@ -47,5 +45,47 @@ slides.forEach((elemento) => {
         <img src="./img/05.webp" alt="">
     </div>
     </div>
-    `;
+`;
+
+let slideActive = 0;
+
+document.getElementById(`next`).addEventListener(`click`, function () {
+	if (slideActive < 4) {
+		slideActive++;
+		slide.innerHTML = stampaSlide();
+	} else {
+		slideActive = 0;
+		slide.innerHTML = stampaSlide();
+	}
 });
+
+document.getElementById(`prev`).addEventListener(`click`, function () {
+	if (slideActive > 0) {
+		slideActive--;
+		slide.innerHTML = stampaSlide();
+	} else {
+		slideActive = 4;
+		slide.innerHTML = stampaSlide();
+	}
+});
+
+// slides.forEach((elemento) => {
+// 	slide.innerHTML = `
+//     <div class="row">
+//     <div class="col-img">
+//         <img src="./img/${elemento.percorso}" alt="">
+//         <div class="absolute">
+//             <h4>${elemento.titolo}</h4>
+//             <p>${elemento.descrizione}</p>
+//         </div>
+//     </div>
+//     <div class="col-thumb">
+//         <img src="./img/01.webp" alt="">
+//         <img src="./img/02.webp" alt="">
+//         <img src="./img/03.webp" alt="">
+//         <img src="./img/04.webp" alt="">
+//         <img src="./img/05.webp" alt="">
+//     </div>
+//     </div>
+//     `;
+// });
